7078a6e9f1e0f2315fd54b93aa361c259dca63f4
taoxy2002 <74699543+taoxy2002@users.noreply.github.com>
2023-04-20 17:17:47 +0800

    Create 7.asm

---
+++ EXAM/7.asm
@@ -0,0 +1,75 @@
+;7.	键盘输入一个十进制数（字类型），以二进制的形式输出。
+DATAS SEGMENT
+    INPUT DW 0
+    OUTPUT DB 16 DUP(0),'$'
+    BUFFER DB 6,5 DUP(0)
+    STRING1 DB 'INPUT DEC:$'
+    STRING2 DB 'OUTPUT HEX:$'
+    MUL_NUM DW 10
+    CR_LF DB 0DH,0AH,'$'
+    FLAG DB 1
+    DIV_NUM DW 2
+DATAS ENDS
+
+STACKS SEGMENT
+    
+STACKS ENDS
+
+CODES SEGMENT
+    ASSUME CS:CODES,DS:DATAS,SS:STACKS
+START:
+    MOV AX,DATAS
+    MOV DS,AX
+    ;输入提示
+    LEA DX,STRING1
+    MOV AH,9
+    INT 21H
+
+    ;一次输入十进制数，分别乘上权重
+	LEA DX,BUFFER
+    MOV AH,0AH
+    INT 21H
+
+    MOV CX,0
+    MOV CL,BUFFER[1]
+    LEA SI,BUFFER[2]
+    ;CX为循环的次数
+INP:
+    MOV AX,INPUT
+    MUL MUL_NUM
+    MOV BL,[SI]
+    SUB BL,30H
+    MOV BH,0
+    ADD AX,BX
+    MOV INPUT,AX
+    INC SI
+
+    LOOP INP
+
+    LEA DX,STRING2
+    MOV AH,9
+    INT 21H
+
+    MOV AX,INPUT
+    MOV CX,16
+    
+    LEA SI,OUTPUT
+    ADD SI,15
+DISP:
+    MOV DX,0
+    DIV DIV_NUM ;对字除法
+    MOV BX,AX
+    ADD DL,30H
+    MOV [SI],DL
+    DEC SI
+    LOOP DISP
+
+    LEA DX,OUTPUT
+    MOV AH,9
+    INT 21H
+
+    MOV AH,4CH
+    INT 21H
+
+CODES ENDS
+    END START
